e673a5c9fd6660b2076dc4ed675a7b6d1846a2a5
Adam Sanderson <netghost@gmail.com>
2025-04-26 13:05:15 -0700

    Set a basepath for the demo site

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    base: '/expanding-textarea/',
     build: {
         rollupOptions: {
             input: {
